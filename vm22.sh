9913de31ed7915932e454d39266325f10922d503
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2024-03-21 06:04:47 +0100

    Update vm22.sh

--- vm22.sh
+++ vm22.sh
@@ -68,7 +68,7 @@ clear
 echo "Script by fb.com/thuong.hai.581"
 echo "Repo: https://github.com/kmille36/Windows-11-VPS"
 
-echo "Windows Server 2012 Datacenter" > abc; echo MicrosoftWindowsServer:WindowsServer:2012-Datacenter:latest > win 
+echo "Windows Server 2022 Datacenter" > abc; echo MicrosoftWindowsServer:WindowsServer:2022-Datacenter:latest > win 
 
 
 goto step3
